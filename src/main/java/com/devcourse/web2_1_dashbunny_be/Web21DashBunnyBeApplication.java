f3b85b27bc401b644a43e83f1111542097c2467e
MagongDo <lpok2401@naver.com>
2024-11-18 12:36:02 +0900

    프로젝트 베이스 커밋

---
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/Web21DashBunnyBeApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.web2_1_dashbunny_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web21DashBunnyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web21DashBunnyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcourse/web2_1_dashbunny_be/Web21DashBunnyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.web2_1_dashbunny_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web21DashBunnyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
