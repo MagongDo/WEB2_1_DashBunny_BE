b3285766eb4f6e1dbc1c4da301e673a760ca7e6c
Hojeong016 <skflsehdn@naver.com>
2024-11-21 16:55:58 +0900

    feat : 가게 관리 controller 베이스

---
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/store/controller/DeliveryInfoController.java
@@ -0,0 +1,5 @@
+package com.devcourse.web2_1_dashbunny_be.feature.owner.store.controller;
+
+//배달 정보 컨트롤러
+public class DeliveryInfoController {
+}
---
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/store/controller/OperationInfoController.java
@@ -0,0 +1,12 @@
+package com.devcourse.web2_1_dashbunny_be.feature.owner.store.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 가게 운영정보 컨트롤러.
+ */
+@RestController
+@RequestMapping("/api/store")
+public class OperationInfoController {
+}
