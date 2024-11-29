bfcf6b998d54024a688bb055ca97b107b5fb9fdd
Hojeong016 <skflsehdn@naver.com>
2024-11-30 00:36:32 +0900

    modify : 메뉴 수정 페이지 dto 메뉴 소개 추가

--- src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/menu/service/MenuService.java
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/menu/service/MenuService.java
@@ -11,16 +11,20 @@ public interface MenuService {
     List<MenuManagement> findGroupMenu(String groupId); // 해당 그룹 메뉴 조회
     List<MenuManagement> findSearchMenuName(String menuName);
 
-    /**
-     * 새로운 메뉴 등록.
-     */
-    void create(String storeId, MenuManagement menu, CreateMenuRequestDto createMenuRequestDto);
-    void updateAll(Long menuId,UpdateMenuRequestDto updateMenuRequestDTO); //단 건 메뉴 모든 정보 수정 (필드 별 수정 가능)
+  /**
+   * 새로운 메뉴 등록.
+   */
+  void create(String storeId, MenuManagement menu, CreateMenuRequestDto createMenuRequestDto);
 
-    /**
-     * 다중 메뉴 품절 처리.
-     */
-    void updateActionIsSoldOut(UpdateActionRequestDto actionRequestDTO);
+  /**
+  *단 건 메뉴 모든 정보 수정 (필드 별 수정 가능).
+  */
+  void updateAll(Long menuId, UpdateMenuRequestDto updateMenuRequestDto);
+
+  /**
+   * 다중 메뉴 품절 처리.
+   */
+  void updateActionIsSoldOut(UpdateActionRequestDto actionRequestDto);
     void updateImage(Long menuId, UpdateMenuImageRequestDto imageUrlDTO); //1페이지 이미지 단 건 수정
     void updateIsSoldOut(Long menuId,UpdateSoldOutRequestDto updateSoldOutRequestDTO);
 
--- src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/menu/service/MenuServiceImpl.java
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/feature/owner/menu/service/MenuServiceImpl.java
@@ -6,42 +6,46 @@ import com.devcourse.web2_1_dashbunny_be.domain.owner.StoreManagement;
 import com.devcourse.web2_1_dashbunny_be.feature.owner.common.Validator;
 import com.devcourse.web2_1_dashbunny_be.feature.owner.dto.menu.*;
 import com.devcourse.web2_1_dashbunny_be.feature.owner.menu.repository.MenuGroupRepository;
-import com.devcourse.web2_1_dashbunny_be.feature.owner.store.repository.StoreManagementRepository;
 import com.devcourse.web2_1_dashbunny_be.feature.owner.menu.repository.MenuRepository;
+import com.devcourse.web2_1_dashbunny_be.feature.owner.store.repository.StoreManagementRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * .
+ */
 @Service
 @RequiredArgsConstructor
 public class MenuServiceImpl implements MenuService {
 
-    private final Validator validator;
-    private final StoreManagementRepository storeManagementRepository;
-    private final MenuRepository menuRepository;
-    private final MenuGroupRepository menuGroupRepository;
+  private final Validator validator;
+  private final StoreManagementRepository storeManagementRepository;
+  private final MenuRepository menuRepository;
+  private final MenuGroupRepository menuGroupRepository;
 
-    // 그룹 메뉴 조회
-    @Override
-    public List<MenuManagement> findGroupMenu(String groupId) {
-        // 그룹 ID에 해당하는 메뉴 리스트 조회
-        return null;/*menuRepository.findByGroupId(groupId);*/
-    }
+  // 그룹 메뉴 조회
+  @Override
+  public List<MenuManagement> findGroupMenu(String groupId) {
+  // 그룹 ID에 해당하는 메뉴 리스트 조회
+    return null;
+  /*menuRepository.findByGroupId(groupId);*/
+  }
 
-    //전체 메뉴 조회(메뉴 관리 1페이지 목록)
-    @Override
-    public List<MenuManagement> findStoreAllMenu(String storeId) {
-        StoreManagement store = validator.validateStoreId(storeId);
-        return menuRepository.findAllByStoreId(storeId);
-    }
+  //전체 메뉴 조회(메뉴 관리 1페이지 목록)
+  @Override
+  public List<MenuManagement> findStoreAllMenu(String storeId) {
+    StoreManagement store = validator.validateStoreId(storeId);
+    return menuRepository.findAllByStoreId(storeId);
+  }
 
-    // 메뉴명 기준으로 메뉴 검색
-    @Override
-    public List<MenuManagement> findSearchMenuName(String menuName) {
-        return null;/*menuRepository.findByMenuNameContaining(menuName);*/
-    }
+  // 메뉴명 기준으로 메뉴 검색
+  @Override
+  public List<MenuManagement> findSearchMenuName(String menuName) {
+    return null;
+    /*menuRepository.findByMenuNameContaining(menuName);*/
+  }
 
   /**
   * 새로운 메뉴 등록을 위한 api service.
@@ -51,91 +55,91 @@ public class MenuServiceImpl implements MenuService {
           String storeId, MenuManagement menu, CreateMenuRequestDto createMenuRequestDto) {
     StoreManagement store = validator.validateStoreId(storeId);
 
-    if(createMenuRequestDto.getMenuGroupId() !=null) {
-        MenuGroup group = validator.validateGroupId(createMenuRequestDto.getMenuGroupId());
-        menu.setMenuGroup(group);
+    if (createMenuRequestDto.getMenuGroupId() != null) {
+      MenuGroup group = validator.validateGroupId(createMenuRequestDto.getMenuGroupId());
+      menu.setMenuGroup(group);
     }
 
     menu.setStoreId(storeId);
     menuRepository.save(menu);
   }
 
-    @Override
-    public void updateAll(Long menuId, UpdateMenuRequestDto updateMenuRequestDTO) {
-        MenuManagement menu = menuRepository.findById(menuId)
+  @Override
+  public void updateAll(Long menuId, UpdateMenuRequestDto updateMenuRequestDto) {
+    MenuManagement menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new RuntimeException("메뉴를 찾을 수 없습니다."));
-        if (updateMenuRequestDTO.getMenuName() != null) {
-            menu.setMenuName(updateMenuRequestDTO.getMenuName());
-        }
-        if (updateMenuRequestDTO.getMenuGroupId() != null) {
-            MenuGroup menuGroup = menuGroupRepository.findById(updateMenuRequestDTO.getMenuGroupId())
-                    .orElseThrow(() -> new RuntimeException("메뉴 그룹을 찾을 수 없습니다."));
-            menu.setMenuGroup(menuGroup);
-        }
-        if (updateMenuRequestDTO.getPrice() != null) {
-            menu.setPrice(updateMenuRequestDTO.getPrice());
-        }
-        if (updateMenuRequestDTO.getMenuContent() != null) {
-            menu.setMenuContent(updateMenuRequestDTO.getMenuContent());
-        }
-        if (updateMenuRequestDTO.getStockAvailable() != null) {
-            menu.setStockAvailable(updateMenuRequestDTO.getStockAvailable());
-        }
-        if (updateMenuRequestDTO.getMenuStock() != null) {
-            menu.setMenuStock(Math.toIntExact(updateMenuRequestDTO.getMenuStock()));
-        }
-        if (updateMenuRequestDTO.getIsSoldOut() != null) {
-            menu.setIsSoldOut(updateMenuRequestDTO.getIsSoldOut());
-        }
-        menuRepository.save(menu);
+    if (updateMenuRequestDto.getMenuName() != null) {
+      menu.setMenuName(updateMenuRequestDto.getMenuName());
     }
+    if (updateMenuRequestDto.getMenuGroupId() != null) {
+      MenuGroup menuGroup = menuGroupRepository.findById(updateMenuRequestDto.getMenuGroupId())
+                    .orElseThrow(() -> new RuntimeException("메뉴 그룹을 찾을 수 없습니다."));
+      menu.setMenuGroup(menuGroup);
+    }
+    if (updateMenuRequestDto.getPrice() != null) {
+      menu.setPrice(updateMenuRequestDto.getPrice());
+    }
+    if (updateMenuRequestDto.getMenuContent() != null) {
+      menu.setMenuContent(updateMenuRequestDto.getMenuContent());
+    }
+    if (updateMenuRequestDto.getStockAvailable() != null) {
+      menu.setStockAvailable(updateMenuRequestDto.getStockAvailable());
+    }
+    if (updateMenuRequestDto.getMenuStock() != null) {
+      menu.setMenuStock(Math.toIntExact(updateMenuRequestDto.getMenuStock()));
+    }
+    if (updateMenuRequestDto.getIsSoldOut() != null) {
+      menu.setIsSoldOut(updateMenuRequestDto.getIsSoldOut());
+    }
+    menuRepository.save(menu);
+  }
 
   /**
   * 다중 메뉴 품절 처리.
   */
   @Override
-  public void updateActionIsSoldOut(UpdateActionRequestDto actionRequestDTO) {
-      for(Long menuId : actionRequestDTO.getMenuIds()){
-          MenuManagement menu = validator.validateMenuId(menuId);
-          menu.setIsSoldOut(true);
-          menuRepository.save(menu);
-      }
+  public void updateActionIsSoldOut(UpdateActionRequestDto actionRequestDto) {
+    for (Long menuId : actionRequestDto.getMenuIds()) {
+      MenuManagement menu = validator.validateMenuId(menuId);
+      menu.setIsSoldOut(true);
+      menuRepository.save(menu);
+    }
   }
 
-    @Override
-    public void updateImage(Long menuId, UpdateMenuImageRequestDto imageUrlDTO) {
-        MenuManagement menu = menuRepository.findById(menuId)
+  @Override
+  public void updateImage(Long menuId, UpdateMenuImageRequestDto imageUrlDto) {
+    MenuManagement menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new RuntimeException("해당 메뉴를 찾을 수 없습니다."));
-        menu.setMenuImage(imageUrlDTO.getImageUrl()); // 이미지 URL 업데이트
-        menuRepository.save(menu); // 변경 내용 저장
-    }
+    menu.setMenuImage(imageUrlDto.getImageUrl()); // 이미지 URL 업데이트
+    menuRepository.save(menu); // 변경 내용 저장
+  }
 
 
-    @Override
-    public void updateIsSoldOut(Long menuId, UpdateSoldOutRequestDto updateSoldOutRequestDTO) {
-        MenuManagement menu = menuRepository.findById(menuId)
+  @Override
+  public void updateIsSoldOut(Long menuId, UpdateSoldOutRequestDto updateSoldOutRequestDto) {
+    MenuManagement menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new RuntimeException("해당 메뉴를 찾을 수 없습니다."));
-        menu.setIsSoldOut(updateSoldOutRequestDTO.getIsSoldOut()); // 품절 상태 업데이트
-        menuRepository.save(menu); // 변경 내용 저장
-    }
+    menu.setIsSoldOut(updateSoldOutRequestDto.getIsSoldOut()); // 품절 상태 업데이트
+    menuRepository.save(menu); // 변경 내용 저장
+  }
 
   /**
    * 다중 메뉴 삭제를 위한 api service.
    */
   @Override
-  public void delete(UpdateActionRequestDto actionRequestDTO) {
-     for(Long menuId : actionRequestDTO.getMenuIds()) {
-        menuRepository.deleteById(menuId);
-     }
+  public void delete(UpdateActionRequestDto actionRequestDto) {
+    for (Long menuId : actionRequestDto.getMenuIds()) {
+      menuRepository.deleteById(menuId);
     }
+  }
 
-    @Override
-    public MenuWithMenuGroupResponseDto MenuWithGroups(Long menuId) {
-        MenuManagement menu = menuRepository.findById(menuId)
+  @Override
+  public MenuWithMenuGroupResponseDto MenuWithGroups(Long menuId) {
+    MenuManagement menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new RuntimeException("메뉴를 찾을 수 없습니다."));
-        List<MenuGroup> menuGroups = menuGroupRepository.findAll();
+    List<MenuGroup> menuGroups = menuGroupRepository.findAll();
 
-        return MenuWithMenuGroupResponseDto.builder()
+    return MenuWithMenuGroupResponseDto.builder()
                 .menu(MenuResponseDto.fromEntity(menu))
                 .menuGroups(menuGroups.stream()
                         .map(group -> new MenuGroupResponseDto(group.getGroupId(), group.getGroupName()))
