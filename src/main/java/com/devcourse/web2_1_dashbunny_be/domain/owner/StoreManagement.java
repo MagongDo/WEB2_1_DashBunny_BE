22ca8c93d77908969f1e0cca99999c38ff264c3a
Hojeong016 <skflsehdn@naver.com>
2024-12-04 17:39:47 +0900

    modify : 쇼츠 대표 메뉴 기능 추가로 인한 로직 변동

--- src/main/java/com/devcourse/web2_1_dashbunny_be/domain/owner/StoreManagement.java
+++ src/main/java/com/devcourse/web2_1_dashbunny_be/domain/owner/StoreManagement.java
@@ -107,7 +107,6 @@ public class StoreManagement {
   //가게 등록 승인 날짜
   private LocalDateTime approvedDate;
 
-
   //스토어가 가진 쿠폰 리스트
   //쿠폰이 없어도 스토어는 생성될 수 있어야한다. 리스트 초기화 진행
   @OneToMany(mappedBy = "storeManagement", cascade = CascadeType.ALL, orphanRemoval = true)
